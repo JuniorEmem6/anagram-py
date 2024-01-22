3637800219d13df0d5e4a65dc38f78be0851bdde
Emem Edem Junior <106032953+JuniorEmem6@users.noreply.github.com>
2024-01-22 13:39:45 +0100

    Update solution.py
    This is the solution to the anagram, I finally found a way to edit the file, thank you for your understanding and I hope to work with your organization.

--- solution.py
+++ solution.py
@@ -1,3 +1,21 @@
 # write the function is_anagram
 def is_anagram(test, original):
-    pass
+    fword = test.lower()
+    sword = original.lower()
+
+    if(len(fword) != len(sword)
+       return False
+
+    new_fword = sorted(fword)
+    new_sword = sorted(sword)
+
+    for count in range(original):
+        if new_fword[count] in new_word:
+            continue
+        else:
+            return False
+
+    return True
+
+solution = is_anagram("foefet", "toffee")
+print(solution)
